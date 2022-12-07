9f921feda46c71a5da3bdfd9a5ed8fe1dc8d97a0
leejiyoung <wldud5117@naver.com>
2022-12-07 11:42:30 +0900

    App.js완성함. (react둘러보기)

--- src/App.js
+++ src/App.js
@@ -13,23 +13,37 @@ class App extends Component{
   render(){
 
     let myName ="김구라";
+
+    //camel case로 작성
+    //요소에 적용할 inline css를 object로 정의하고 이것을 요소에 적용할 수 있다.
+    const boxStyle={
+      width:"100px",
+      height:"100px",
+      border:"1px solid red",
+      backgroundColor:"yellow"
+    };
+
     return(
       <div className="container">
-      <h1>인덱스 페이지입니다.</h1>
-      {/* myName이라는 변수 안에 있는 내용 사용하기 */}
-      <p>내 이름은 <strong>{myName}</strong></p>
-      {/*
-      [이벤트 처리]
+        <h1>인덱스 페이지입니다.</h1>
+        {/* myName이라는 변수 안에 있는 내용 사용하기 */}
+        <p>내 이름은 <strong>{myName}</strong></p>
+        {/*
+        [이벤트 처리]
 
-      onEventName = {화살표 함수}
+        onEventName = {화살표 함수}
 
-      처럼 on 다음에 이벤트 명을 camel case로 작성한다.
-      화살표 함수는 해당 이벤트가 발생하면 자동으로 호출된다.
-      ex) onClick, onMouseOver, onChange, onInput, onKeyDown, onFocus...
-      */}
-      <button className='btn btn-primary' onClick={()=>{
-        alert("눌렀다.");
-      }}>눌러보셈</button>
+        처럼 on 다음에 이벤트 명을 camel case로 작성한다.
+        화살표 함수는 해당 이벤트가 발생하면 자동으로 호출된다.
+        ex) onClick, onMouseOver, onChange, onInput, onKeyDown, onFocus...
+        */}
+        <button className='btn btn-primary' onClick={()=>{
+          alert("눌렀다.");
+        }}>눌러보셈</button>
+
+        <div style={boxStyle}>
+          div
+        </div>
       </div>
     );
   }
