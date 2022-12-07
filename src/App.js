b0c0291c90a4793c49a7b6b7c09d431a513f592b
leejiyoung <wldud5117@naver.com>
2022-12-07 11:08:41 +0900

    bootstrap css로딩 app.js수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//boot strap css로딩하기
+ // 방법1: index.html에 부트스트랩 css외부링크를 단다.
+ // 방법2: bootstrap 폴더를 다운받아 여기서 import한다.
 
 function App() {
+  
+  let myName = "김구라";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>인덱스 페이지입니다.</h1>
+      {/* myName이라는 변수 안에 있는 내용 사용하기 */}
+      <p>내 이름은 <strong>{myName}</strong></p>
+      <button className='btn btn-primary'>눌러보셈</button>
     </div>
   );
 }
